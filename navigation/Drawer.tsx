823f8cb4ff005c0d8d8e4b797d0b85214d2d8c7e
Richard Au <au.richard4@gmail.com>
2022-10-11 11:47:04 -0700

    Adding Header component to NavDrawer to make it appear above Drawer header.

--- navigation/Drawer.tsx
+++ navigation/Drawer.tsx
@@ -2,11 +2,13 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import About from "../components/About";
 import Home from "../components/Home";
+import Header from "../components/Header";
 
 const Drawer = createDrawerNavigator();
 export default function NavDrawer() {
   return (
     <NavigationContainer>
+      <Header />
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="About" component={About} />
