fbe3aaa879c6a9070f59d6d8273cb6f753e52047
Richard Au <au.richard4@gmail.com>
2022-10-10 22:03:42 -0700

    Creating new Home page component.

---
+++ components/Home.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import Header from "./Header";
+import Footer from "./Footer";
+import LocationSearchHome from "./LocationSearchHome";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Text>This is the Home Page.</Text>
+      <LocationSearchHome />
+      <Footer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
