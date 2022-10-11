31fcc4f55c82dc7a022f76f608b411bc60726bdc
Richard Au <au.richard4@gmail.com>
2022-10-11 11:45:13 -0700

    Commenting out Header component in return.

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 export default function About() {
   return (
     <View style={styles.container}>
-      <Header />
+      {/* <Header /> */}
       <Text>This is the About Page.</Text>
       <Footer />
     </View>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,35 +1,44 @@
-import { StyleSheet, TouchableOpacity, Text, View, Image, TextInput, Button, FlatList } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Header() {
   return (
-    <View style={styles.header}>       
-        <FontAwesome name='navicon' color="#B74F6F" size={32} />
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>Comb</Text>             
-          <Image 
-            source={require('../assets/images/comb_logo.png')}            
-            style={styles.image} 
-          />          
-        </View>      
+    <View style={styles.header}>
+      <FontAwesome name="navicon" color="#B74F6F" size={32} />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Comb</Text>
+        <Image
+          source={require("../assets/images/comb_logo.png")}
+          style={styles.image}
+        />
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create ({
-  header: {    
-    flexDirection: 'row',        
-    backgroundColor: '#031926',
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    backgroundColor: "#031926",
     padding: 10,
-    alignItems: 'center'    
+    alignItems: "center",
   },
   titleContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    flexGrow: 1,    
+    flexDirection: "row",
+    justifyContent: "center",
+    flexGrow: 1,
   },
   title: {
-    color: '#B74F6F',
+    color: "#B74F6F",
     fontSize: 40,
     padding: 10,
   },
@@ -38,10 +47,9 @@ const styles = StyleSheet.create ({
   },
   image: {
     padding: 10,
-    width: 50,    
+    width: 50,
     height: 70,
-    borderColor: 'white',
-    borderWidth: 3,  
-  }, 
-
+    borderColor: "white",
+    borderWidth: 3,
+  },
 });
