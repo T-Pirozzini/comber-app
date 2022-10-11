ad506ebed19105b2024e611d5f99a5a2102ee23a
Richard Au <au.richard4@gmail.com>
2022-10-10 22:04:13 -0700

    Creating new About page component.

---
+++ components/About.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Text>This is the About Page.</Text>
+      <Footer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
