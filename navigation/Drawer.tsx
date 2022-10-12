387a0d2de0c6228dbfbd735437d244b5417d2e6c
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-10-11 18:06:50 -0700

    modify navigator drawer styles

--- navigation/Drawer.tsx
+++ navigation/Drawer.tsx
@@ -1,3 +1,4 @@
+import { Button, View, Text, StyleSheet, Image, SafeAreaView } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import About from "../components/About";
@@ -7,12 +8,50 @@ import Header from "../components/Header";
 const Drawer = createDrawerNavigator();
 export default function NavDrawer() {
   return (
-    <NavigationContainer>
-      <Header />
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} />
+    <NavigationContainer style={styles.heading}>
+      {/* <Header /> */}
+        <View>
+          <Text>DO ANYTHING</Text>
+        </View>
+      <Drawer.Navigator 
+        initialRouteName="Home"
+        // drawerContent={<Image
+        //   source={require("../assets/images/comb_logo.png")}
+        //   style={{ width: 50, height: 50}}
+        // />}
+        // <Icon name="Comb logo" source={"../assets/images/comb_logo.png"} />
+      >
+        <Drawer.Screen 
+                 
+          name="Home" 
+          component={Home} 
+          options={{
+            title: 'Comb',
+            headerTitleAlign: 'center',
+            headerTintColor: "#B74F6F",
+            headerBackground: () => (
+              <Image
+              source={require("../assets/images/comb_logo.png")}
+              style={{ width: 50, height: 50, backgroundColor: '#031926', }}
+              // style={StyleSheet.absoluteFill}
+              />          
+              ),
+              // headerStyle: {
+              //   backgroundColor: '#031926',              
+              // },
+            }}             
+            
+           />
         <Drawer.Screen name="About" component={About} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    color: 'red',
+  }
+
+
+})
