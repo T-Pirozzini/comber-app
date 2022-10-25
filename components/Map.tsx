5d274925af0e7e6e8d581629a2bbbf0316f39fe7
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-10-25 13:43:43 -0700

    style location search

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,21 +1,7 @@
 import React, { useState, useRef, useEffect, useLayoutEffect } from "react";
-import MapView, {
-  Callout,
-  Marker,
-  Polygon,
-  PROVIDER_GOOGLE,
-} from "react-native-maps";
+import MapView, {  Callout,  Marker,  Polygon,  PROVIDER_GOOGLE,} from "react-native-maps";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import {
-  Button,
-  View,
-  Text,
-  StyleSheet,
-  Dimensions,
-  TouchableOpacity,
-  Image,
-  TextInput,
-} from "react-native";
+import { Button,  View,  Text,  StyleSheet,  Dimensions,  TouchableOpacity,  Image,  TextInput,} from "react-native";
 import { MaterialIcons, AntDesign } from "@expo/vector-icons";
 import * as Location from "expo-location";
 
@@ -170,13 +156,16 @@ export default function Map() {
   };
   return (
     <View style={styles.container}>
+
+      {/* Google Places Autocomplete Component       */}
       <GooglePlacesAutocomplete
         ref={googlePlacesAutoCompleteRef}
-        placeholder="Search"
-        fetchDetails={true}
+        placeholder="Enter a City"
+        fetchDetails={true}        
         GooglePlacesSearchQuery={{
           rankby: "distance",
         }}
+        renderLeftButton={()  => <Image source={require('https://i.ya-webdesign.com/images/transparent-pin-google-2.png')} style={styles.tinylogo} />}
         onPress={(data, details = null) => {
           // Trying to test animating to clicked on location
           setRegionGoogleMap({
@@ -231,7 +220,7 @@ export default function Map() {
 
                 // black background, black circle X
                 name={"closecircle"}
-                color={"black"}
+                color={"#7DD181"}
                 size={20}
                 style={styles.clearButton}
               />
@@ -245,11 +234,33 @@ export default function Map() {
             top: 10,
             width: "80%",
             zIndex: 1,
+            backgroundColor: 'rgba(3, 25, 38, .8)',
+            margin: 5,
+            marginTop: 0,
+            borderRadius: 10,
           },
-          listView: { backgroundColor: "white" },
-        }}
-      />
+          textInputContainer: {            
+            padding: 5,
+            paddingBottom: 1,            
+          },
+          textInput: {
+            height: 38,
+            color: '#5d5d5d',
+            fontSize: 20,                     
+          },          
+          description: {
+            color: 'white',
+            fontSize: 16,            
+          },
+          row: {
+            backgroundColor: 'rgba(3, 25, 38, .1)',
+          },
+          powered: {
 
+          }     
+        }}
+      />  
+        
       <MapView
         provider={PROVIDER_GOOGLE}
         showsCompass={true}
@@ -355,4 +366,9 @@ const styles = StyleSheet.create({
     paddingLeft: 5,
     paddingTop: 10,
   },
+  tinylogo: {
+    width: 40,
+    height: 20,
+  }
+  
 });
