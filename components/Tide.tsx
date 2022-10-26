971a294c414bc36a9e845c720a3a858c10c71946
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-10-25 19:25:03 -0700

    clean up tide component console logs and formatting - then test application

--- components/Tide.tsx
+++ components/Tide.tsx
@@ -2,40 +2,43 @@ import React, { useState } from 'react'
 import { Button, View, Text, StyleSheet } from "react-native";
 
 export default function Tide() {
+  
+  const [currentStationId, setCurrentStationId] = useState("")
+  const [stationName, setStationName] = useState("")
+  const [stationLat, setStationLat] = useState("")
+  const [stationLng, setStationLng] = useState("")
+  const [waveHeight, setWaveHeight] = useState("") 
 
-  const [locations, setLocations] = useState('')
-  const [currentStationId, setCurrentStationId] = useState("5cebf1df3d0f4a073c4bbbf3")
-  const [stationName, setStationName] = useState("none")
-  const [stationLat, setStationLat] = useState("none")
-  const [stationLng, setStationLng] = useState("none")
-  const [waveHeight, setWaveHeight] = useState("none")
-
+  
+  // API CALL #1: Get Tide Station Information: ID, Name and Coords - Then set state
   const getTideStationInfo = async () => {
     try {
       const response = await fetch(
         'https://api-iwls.dfo-mpo.gc.ca/api/v1/stations?chs-region-code=PAC'
       );
-      const json = await response.json();
+      const stationData = await response.json();
       // console.log(json)
-      for (let i = 0; i < json.length; i++) {
-        if (json[i].officialName == 'Nanaimo Harbour') {          
-          console.log("STATION NAME", json[i].officialName)
-          console.log("STATION ID", json[i].id)
-          console.log("LATITUDE",json[i].latitude)
-          console.log("LONGITIDUE",json[i].longitude)
+      for (let i = 0; i < stationData.length; i++) {
+        // Currently set to Nanaimo Harbour - Set this to dynamic city variable based on search
+        if (stationData[i].officialName == 'Nanaimo Harbour') {          
+          // console.log("STATION NAME", stationData[i].officialName)
+          // console.log("STATION ID", stationData[i].id)
+          // console.log("LATITUDE",stationData[i].latitude)
+          // console.log("LONGITIDUE",stationData[i].longitude)
           
-          setCurrentStationId(json[i].id)
-          setStationName(json[i].officialName)
-          setStationLat(json[i].latitude)
-          setStationLng(json[i].longitude)
+          setCurrentStationId(stationData[i].id)
+          setStationName(stationData[i].officialName)
+          setStationLat(stationData[i].latitude)
+          setStationLng(stationData[i].longitude)
         }
-      }    
-      // return json;
+      }
+      // return stationData   
     } catch (error) {
       console.error(error);
     }
   };
 
+  // API CALL #2: Get Wave height information based on station id - If current station matches station id, return wave height
   const getWaveHeightInfo = async () => {
     try {
       const response = await fetch(
@@ -51,12 +54,9 @@ export default function Tide() {
           // console.log("stationids2", tideData[i].stationId)
           console.log("CURRENT WAVE HEIGHT", tideData[i].measurementPublicDTOs[i].value)
           setWaveHeight(tideData[i].measurementPublicDTOs[i].value)            
-        }            
-        // for (let j = 0; j < tideData[i].stationId.length; j++) {
-        //   console.log("WAVE HEIGHT", tideData[i].measurementPublicDTOs[j].value) 
-        // }                      
+        }                           
       }         
-      return tideData;
+      // return tideData;
     } catch (error) {
       console.error(error);
     }
@@ -69,25 +69,23 @@ export default function Tide() {
         title="tide-Station-Info"
         onPress={getTideStationInfo}
       />
-      <Button
-        title="Wave-Height-Info"
-        onPress={getWaveHeightInfo}
-      />
       <Text>
         {"STATION NAME: " + stationName}
         {"\n"}
         {"STATION LAT: " + stationLat}
         {"\n"}
-        {"STATION LNG: " + stationLng}
-        {"\n"}
-        {"STATION CURRENT WAVE HEIGHT: " + waveHeight}
+        {"STATION LNG: " + stationLng}        
       </Text>
-      
-    </View>
-    
+      <Button
+        title="Wave-Height-Info"
+        onPress={getWaveHeightInfo}
+      />
+       <Text>       
+        {"STATION CURRENT WAVE HEIGHT: " + waveHeight}
+      </Text>      
+    </View>    
   )
 }
-
 
 const styles = StyleSheet.create({
   container: {
