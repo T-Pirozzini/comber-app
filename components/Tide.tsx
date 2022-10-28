0954f6d3ed7eb724c8f4917642c3052b7ebe9b8e
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-10-27 19:56:40 -0700

    retreive wave and tide station info whenever the current city changes - version 1 of tide component complete

--- components/Tide.tsx
+++ components/Tide.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet } from "react-native";
-import { Button, SpeedDial } from "@rneui/themed";
+import React, { useState, useEffect } from 'react'
+import { SpeedDial } from "@rneui/themed";
 
 export default function Tide({city}) {
   
@@ -12,6 +11,10 @@ export default function Tide({city}) {
   const [stationLng, setStationLng] = useState("")
   const [waveHeight, setWaveHeight] = useState("") 
 
+  useEffect(() => {
+    getTideStationInfo();
+    getWaveHeightInfo();
+  },[city])
   
   // API CALL #1: Get Tide Station Information: ID, Name and Coords - Then set state
   const getTideStationInfo = async () => {
@@ -19,26 +22,23 @@ export default function Tide({city}) {
       const response = await fetch(
         'https://api-iwls.dfo-mpo.gc.ca/api/v1/stations?chs-region-code=PAC'
       );
-      const stationData = await response.json();
-      console.log("DATA", json)
-      for (let i = 0; i < stationData.length; i++) {
-        // Currently set to Nanaimo Harbour - Set this to dynamic city variable based on search
-        if (stationData[i].officialName == 'Nanaimo Harbour') {          
-          // console.log("STATION NAME", stationData[i].officialName)
-          // console.log("STATION ID", stationData[i].id)
-          // console.log("LATITUDE",stationData[i].latitude)
-          // console.log("LONGITIDUE",stationData[i].longitude)
-          
+      const stationData = await response.json();     
+      // loop through all stationData
+      for (let i = 0; i < stationData.length; i++) {        
+        // if city is is longer than one word, city equals only the first word
+        if (city.split(" ").length > 1) {
+          city = city.split(' ')[0]          
+        }
+        // Set state if current city matches station Name
+        if (stationData[i].officialName.split(" ")[0].toLowerCase() == `${city}`) {         
           setCurrentStationId(stationData[i].id)
           setStationName(stationData[i].officialName)
           setStationLat(stationData[i].latitude)
           setStationLng(stationData[i].longitude)
-        }
+        } 
       }
-      // return stationData   
-    } catch (error) {
-      console.error(error);
-    }
+      // return stationData  -- need an error message for the user if the city station does not exist  
+    } catch (error) {console.log("ERROR", error)}    
   };
 
   // API CALL #2: Get Wave height information based on station id - If current station matches station id, return wave height
@@ -47,89 +47,64 @@ export default function Tide({city}) {
       const response = await fetch(
         'https://api-iwls.dfo-mpo.gc.ca/api/v1/stations/data/latest'
       );
-      const tideData = await response.json();
-      // console.log("CURRENT STATION JSON:", tideData)
-      for (let i = 0; i < tideData.length; i++) {
-        // console.log("STATION IDs", tideData[i].stationId)        
-        // console.log("CURRENT STATION ID:", currentStationId)        
-        if (tideData[i].stationId == currentStationId) {
-          // console.log("MATCH!!!!")
-          // console.log("stationids2", tideData[i].stationId)
-          console.log("CURRENT WAVE HEIGHT", tideData[i].measurementPublicDTOs[i].value)                   
+      const tideData = await response.json();      
+      // loop through all canadian tide station data
+      for (let i = 0; i < tideData.length; i++) {        
+        // if a tide station matches the current stations ID, return wave height        
+        if (tideData[i].stationId == currentStationId) {        
           setWaveHeight(tideData[i].measurementPublicDTOs[i].value)            
         }                           
       }         
-      // return tideData;
+      // Display an error if no tide data or match -- set up a notification for the user
     } catch (error) {
       console.error(error);
     }
-  };
- 
+  }; 
 
-  return (
-    // <View>
-    //   <Button
-    //     title="tide-Station-Info"
-    //     onPress={getTideStationInfo}
-    //   />
-    //   {/* <Text>
-    //     {"STATION NAME: " + stationName}
-    //     {"\n"}
-    //     {"STATION LAT: " + stationLat}
-    //     {"\n"}
-    //     {"STATION LNG: " + stationLng}        
-    //   </Text> */}
-    //   <Button
-    //     title="Wave-Height-Info"
-    //     onPress={getWaveHeightInfo}
-    //   />
-    //    {/* <Text>       
-    //     {"STATION CURRENT WAVE HEIGHT: " + waveHeight}
-    //   </Text> */}
-
-      <SpeedDial
-        isOpen={open}
-        icon={{ name: 'waves', type: "material-community", color: '#B74F6F' }}
-        openIcon={{ name: 'close', color: '#B74F6F' }}
-        onOpen={() => setOpen(!open)}
-        onClose={() => setOpen(!open)}
-        overlayColor="rgba(235, 213, 211, 0.0)" // make overlay transparent
+  return (  
+    <SpeedDial
+      isOpen={open}
+      icon={{ name: 'waves', type: "material-community", color: '#B74F6F' }}
+      openIcon={{ name: 'close', color: '#B74F6F' }}
+      onOpen={() => setOpen(!open)}
+      onClose={() => setOpen(!open)}
+      overlayColor="rgba(235, 213, 211, 0.0)" // make overlay transparent
+      color="#031926"
+      placement="right"
+      size="small" 
+    >
+      <SpeedDial.Action
+        icon={{ name: 'location', type: 'entypo', color: '#7DD181' }}
+        title={"Station: " + stationName}
+        titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
+        onPress={() => console.log('Add Something')}
         color="#031926"
-        placement="right"
-        size="small" 
-      >
-        <SpeedDial.Action
-          icon={{ name: 'location', type: 'entypo', color: '#7DD181' }}
-          title={"Station: " + stationName}
-          titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
-          onPress={() => console.log('Add Something')}
-          color="#031926"
-          containerStyle={{margin: -15}}         
-        />        
-        <SpeedDial.Action
-          icon={{name: 'latitude', type: "material-community", color: '#7DD181' }}
-          title={"Latitude: " + stationLat}
-          titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
-          onPress={() => console.log('Delete Something')}          
-          color="#031926"
-          containerStyle={{margin: -15}}
-        />    
-        <SpeedDial.Action
-          icon={{name: 'longitude', type: "material-community", color: '#7DD181' }}
-          title={"Longitude: " + stationLng}
-          titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
-          onPress={() => console.log('Delete Something')}
-          color="#031926"
-          containerStyle={{margin: -15}}
-        />   
-        <SpeedDial.Action
-          icon={{name: 'waves-arrow-up', type: "material-community", color: '#7DD181' }}
-          title={"Wave Height: " + waveHeight}
-          titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
-          onPress={() => console.log('Delete Something')}
-          color="#031926"
-          containerStyle={{margin: -15}}
-        />     
-      </SpeedDial>           
+        containerStyle={{margin: -15}}         
+      />        
+      <SpeedDial.Action
+        icon={{name: 'latitude', type: "material-community", color: '#7DD181' }}
+        title={"Latitude: " + stationLat}
+        titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
+        onPress={() => console.log('Delete Something')}          
+        color="#031926"
+        containerStyle={{margin: -15}}
+      />    
+      <SpeedDial.Action
+        icon={{name: 'longitude', type: "material-community", color: '#7DD181' }}
+        title={"Longitude: " + stationLng}
+        titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
+        onPress={() => console.log('Delete Something')}
+        color="#031926"
+        containerStyle={{margin: -15}}
+      />   
+      <SpeedDial.Action
+        icon={{name: 'waves-arrow-up', type: "material-community", color: '#7DD181' }}
+        title={"Wave Height: " + waveHeight}
+        titleStyle = {{backgroundColor: "rgba(3, 25, 38, 1)", color: "#7DD181" }}
+        onPress={() => console.log('Delete Something')}
+        color="#031926"
+        containerStyle={{margin: -15}}
+      />     
+    </SpeedDial>            
   )
 }
