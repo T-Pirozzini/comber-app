e0a205125a2dff9c21f70fbdeb3363e531249d5d
Richard Au <au.richard4@gmail.com>
2022-11-03 16:32:54 -0700

    Fixing Horse clam card and adding size info.

--- components/Info.tsx
+++ components/Info.tsx
@@ -372,13 +372,9 @@ export default function Info() {
             }}
           />
           <Text style={{ marginBottom: 10 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Oval white coloured with brown and black patches worn off shell.
+            Concentric rings on shell.{"\n"}
+            {"\n"}Size: Up to 20cm
           </Text>
           <View style={styles.counter}>
             <Button
