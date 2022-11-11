c19077a1cd37e367855ea772d6862a614904cef1
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-11-10 16:01:40 -0800

    implement message board feature

--- screens/MessageBoard.tsx
+++ screens/MessageBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   TouchableOpacity,
@@ -10,79 +10,98 @@ import {
   FlatList,
 } from "react-native";
 
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, orderBy } from "firebase/firestore";
 import { db, auth } from "../firebase/firebase-config";
 import moment from "moment";
+import { setBackgroundColorAsync } from "expo-system-ui";
+import { RecaptchaVerifier } from "firebase/auth";
+import { Card } from "@rneui/themed";
+import { ScrollView } from "react-native-gesture-handler";
 
-const currentTime = String(moment().fromNow());
+
+const getTime = () => {
+  return moment().format('LLL');
+}
+// const currentTime = moment().format('LLL');
 
 export default function MessageBoard() {
-  const [text, setText] = useState("");
-  const [user, setUser] = useState("")
-  const [time, setTime] = useState("")
-  const [comment, setComment] = useState("")
-
-  // const [userPost, setUserPost] = useState("");
-
-  const posts: object[] = [];
-
+  const [text, setText] = useState("");   
+  const [data, setData] = useState(null)
+     
   const addPost = async () => {
     try {
       const docRef = await addDoc(collection(db, "comments"), {
         currentUser: auth.currentUser?.email,
-        timestamp: currentTime,
+        timestamp: getTime(),
         postbox: text,
       });
-      console.log(currentTime);
-      console.log(auth.currentUser?.email);
+      // console.log(currentTime);
+      // console.log(auth.currentUser?.email);
       console.log("Document written with ID: ", docRef.id);
+      getPosts();
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   };
-
+  
   const getPosts = async () => {
-    const querySnapshot = await getDocs(collection(db, "comments"));
-    let submittedPosts = {};
-    querySnapshot.forEach((doc) => {      
-        setUser(`${doc.id} => ${doc.data().currentUser}`)
-        setTime(`${doc.id} => ${doc.data().timestamp}`)
-        setComment(`${doc.id} => ${doc.data().text}`)
-        console.log(`${doc.id} => ${doc.data().timestamp}`);
-        // console.log(`${doc.id} => ${doc.data().timestamp}`);
-        // console.log(`${doc.id} => ${doc.data().postbox}`);
-        submittedPosts = {user: `${doc.data().currentUser.email}`, timestamp: `${doc.data().currentTime}`, postbox: `${doc.data().text}`}
-        posts.push(submittedPosts)
-    })         
-  };
+    const q = query(collection(db, "comments"), orderBy("timestamp", "desc"))
+    try {
+    const querySnapshot = await getDocs(q);
+    let results = [];    
+    querySnapshot.forEach((doc) => {  
+      results.push({ id: doc.id, ...doc.data() })
+    });
+    setData(results)
+    } catch (e) {
+    console.error("Error adding document: ", e);
+    }
+  }
 
-  // getPosts();
+  useEffect(() => {
+    getPosts();
+  }, [])
+  
+  const renderItem = ({ item }) => (    
+    <ScrollView>
+      <Card>    
+        <Text style={{textAlign: "center", fontWeight: "700"}}>{item.currentUser}</Text>
+        <Card.Divider />
+        <Text>{item.postbox}</Text>    
+        <Card.Divider />
+        <Text style={{fontSize: 12}}>{item.timestamp.toString()}</Text>
+      </Card>
+    </ScrollView>
+  );
 
   return (
     <View style={styles.postContainer}>
-      <Text style={styles.text}>Add a Comment</Text>
 
-      <TextInput
-        placeholder="enter a comment here"
-        value={text}
-        onChangeText={(text) => setText(text)}
-      ></TextInput>
-      <TouchableOpacity onPress={addPost}>
-        <Text>Submit</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={getPosts}>
-        <Text>Get Docs</Text>
-      </TouchableOpacity>
-      <Text>{user}</Text>
-      <Text>{time}</Text>
-      <Text>{comment}</Text>
-      {/* <FlatList
-        data={posts}
-      />        */}
-          
-    </View>
+       <View>
+        <Text style={styles.text}>Add a Comment</Text>
+        <TextInput
+          placeholder="enter a comment here"
+          value={text}
+          onChangeText={(text) => setText(text)}
+        ></TextInput>
+        <TouchableOpacity onPress={addPost}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </View>   
+
+      <View>
+      {data && (
+        <FlatList
+          data={data}
+          renderItem={renderItem}          
+        />
+      )}        
+      </View>     
+
+    </View>    
   );
 }
+
 
 const styles = StyleSheet.create({
   text: {
@@ -90,5 +109,7 @@ const styles = StyleSheet.create({
     color: "#FF5733",
     // justifyContent: "center",
   },
-  postContainer: {},
+  postContainer: {
+    flex: 1,
+  },  
 });
